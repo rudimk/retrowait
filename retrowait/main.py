b412987d820208d296666a4bf05b4413e48c2126
Rudraksh MK <rudraksh@paperkite.in>
2016-12-23 10:10:59 +0530

    Added the main module. Added a class with a constructor that uses the XKCD time accelerator algorithm to calculate the corresponding date for a task's progress.

---
+++ retrowait/main.py
@@ -0,0 +1,20 @@
+from __future__ import division
+import arrow
+import wikipedia
+import math
+
+class Wait:
+    progress = None
+    current_date = arrow.now()
+    back_date = None
+    progress_in_years = None
+    year = None
+
+    def __init__(self, progress):
+        self.progress = progress/100
+        self.progress_in_years = math.exp(20.344*(self.progress**3)+3) - math.exp(3)
+        self.back_date = self.current_date.replace(years=-int(self.progress_in_years))
+        self.year = self.back_date.year
+        
+
+
